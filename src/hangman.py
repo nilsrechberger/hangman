6f2541ded0ba9ca2c0a708de2ce9fa54ecc484be
Nils Rechberger <nilsrechberger@outlook.com>
2026-04-09 16:41:25 +0200

    feat: print hangman img

--- src/hangman.py
+++ src/hangman.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from os import system
+
 class WordBoard():
     def __init__(self, secret) -> None:
         self.secret = secret
@@ -17,23 +19,28 @@ class WordBoard():
     def __str__(self) -> str:
         return " ".join(self.wordBoard)
 
+def show_hangman(attempt: int):
+    system(f'cat img/hangman_{attempt}.txt')
+
 def init_game() -> str:
     secret = input('Please set a secret word: ')
     wordBoard = WordBoard(secret=secret)
+    show_hangman(attempt=0)
     return secret, wordBoard
 
-def check_guess(guess: str, secret: str) -> True | False:
+def check_guess(guess: str, secret: str, attempt) -> True | False:
     if guess in list(secret):
         print(True)
     else:
-        print(False)
+        show_hangman(attempt=attempt)
 
 def main() -> None:
     secret, wordBoard = init_game()
     print(wordBoard)
-    for _ in range(3): # Set max attempts here
+    for attempt in range(3): # Set max attempts here
         guess = input('Please set a guess: ')
-        check_guess(guess=guess, secret=secret)
+        check_guess(guess=guess, secret=secret, attempt=attempt)
+        print(wordBoard)
 
 if __name__ == '__main__':
     main()
