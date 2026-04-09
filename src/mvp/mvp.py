fa53f62888f9606b3abfdc1dc671e0becc981692
Nils Rechberger <nilsrechberger@outlook.com>
2026-04-09 15:29:58 +0200

    feat: enable user inputs

--- src/mvp/mvp.py
+++ src/mvp/mvp.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 
-secret = "cat"
+def init_game() -> str:
+    secret = input('Please set a secret word: ')
+    return secret
 
-guesses = [
-    "c",
-    "x",
-    "a",
-    "t"
-]
-
-def test() -> None:
-    print(list(secret))
+def check_guess(guess: str, secret: str) -> True | False:
+    if guess in list(secret):
+        print(True)
+    else:
+        print(False)
 
 def main() -> None:
-    for letter in guesses:
-        if letter in list(secret):
-            print(True)
-        else:
-            print(False)
+    secret = init_game()
+    for _ in range(3): # Set max attempts here
+        guess = input('Please set a guess: ')
+        check_guess(guess=guess, secret=secret)
 
 if __name__ == '__main__':
     main()
